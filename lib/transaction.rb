03a0d4f3d7105167436e43e2bf5180a4b572114a
Nimna Ekanayaka <nimna@nimna.me>
2016-05-09 11:00:54 -0300

    Transactions class updated.

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,2 +1,14 @@
 class Transaction
+  attr_accessor :customer, :product
+  attr_reader :id
+
+  @@id = 1
+  @@transactions = []
+
+  def initialize(params)
+    @id = @@id
+    @@id++
+    @customer = params[:customer]
+    @product = params[:product]
+  end
 end
