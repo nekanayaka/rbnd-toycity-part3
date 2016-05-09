b002feee1e90fc2d049aa01033a6346a85a26a9b
Nimna Ekanayaka <nimna@nimna.me>
2016-05-09 10:59:48 -0300

    Added the required methods to the customer class.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,2 +1,22 @@
 class Customer
+  attr_accessor :name
+  @@customers = []
+
+  def initialize(params)
+    @name = params[:name]
+
+    if @@customers.find {|customer| @name == customer.name}
+      raise DuplicateCustomerError, "#{@name} already exists."
+    else
+      @@customers << self
+    end
+  end
+
+  def self.find_by_name(name)
+    @@customers.find {|customer| name == customer.name}
+  end
+
+  def self.all
+    @@customers
+  end
 end
