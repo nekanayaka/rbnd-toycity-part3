ac6dba701cb9ee06b05e728f94a11fcf4f7b4ae5
Nimna <nimna@nimna.me>
2016-05-09 12:01:45 +0000

    Updated the product title compare statement and added the find_by_title method.

--- lib/product.rb
+++ lib/product.rb
@@ -8,17 +8,33 @@ class Product
         @price = params[:price]
         @stock = params[:stock]
         
-        for product in @@products
-            if @title == product.title
-                raise DuplicateProductError, "#{@title} already exists."
-            else
-                @@products << self
-            end
+        # @@products.each_with_index do |product, index|
+        #     if @title == product.title && product.title != nil
+        #         raise DuplicateProductError, "#{@title} already exists."
+        #     else
+        #         @@products << self
+        #     end
+        # end
+        
+        if @@products.map { |product| product.title}.include? @title
+            raise DuplicateProductError, "#{@title} already exists."
+        else
+            @@products << self
         end
         
     end
     
     def self.all
         @@products
+    end
+    
+    def find_by_title(title)
+        product = self
+        for prod in @@products
+            if title == prod.title
+                product = prod
+            end
+        end
+        product
     end
 end
