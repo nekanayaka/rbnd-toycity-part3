1fe73705a6f27597ddae253b43feb01a0dc99642
Nimna Ekanayaka <nimna@nimna.me>
2016-05-09 09:24:57 -0300

    Implemented the find method to find if the product exist in the products class array.

--- lib/product.rb
+++ lib/product.rb
@@ -1,33 +1,25 @@
 class Product
     attr_accessor :title, :price, :stock
-    
+
     @@products = []
-    
+
     def initialize(params)
         @title = params[:title]
         @price = params[:price]
         @stock = params[:stock]
-        
-        # @@products.each_with_index do |product, index|
-        #     if @title == product.title && product.title != nil
-        #         raise DuplicateProductError, "#{@title} already exists."
-        #     else
-        #         @@products << self
-        #     end
-        # end
-        
-        if @@products.map { |product| product.title}.include? @title
-            raise DuplicateProductError, "#{@title} already exists."
+
+        if @@products.find {|product| @title == product.title}
+          raise DuplicateProductError, "#{@title} already exists."
         else
-            @@products << self
+          @@products << self
         end
-        
+
     end
-    
+
     def self.all
         @@products
     end
-    
+
     def find_by_title(title)
         product = self
         for prod in @@products
