a79e6e34deaedfc724fad95a054d44d2aa1ba921
Nimna Ekanayaka <nimna@nimna.me>
2016-05-09 10:00:49 -0300

    Implemented all the methods for the product class and launcher is updated accordingly.

--- lib/product.rb
+++ lib/product.rb
@@ -4,29 +4,37 @@ class Product
     @@products = []
 
     def initialize(params)
-        @title = params[:title]
-        @price = params[:price]
-        @stock = params[:stock]
+      @title = params[:title]
+      @price = params[:price]
+      @stock = params[:stock]
 
-        if @@products.find {|product| @title == product.title}
-          raise DuplicateProductError, "#{@title} already exists."
-        else
-          @@products << self
-        end
+      if @@products.find {|product| @title == product.title}
+        raise DuplicateProductError, "#{@title} already exists."
+      else
+        @@products << self
+      end
 
     end
 
     def self.all
-        @@products
+      @@products
     end
 
-    def find_by_title(title)
-        product = self
-        for prod in @@products
-            if title == prod.title
-                product = prod
-            end
+    def self.find_by_title(title)
+      @@products.find {|product| title == product.title}
+    end
+
+    def in_stock?
+      self.stock != 0
+    end
+
+    def self.in_stock
+      products_with_stock = []
+      for product in @@products
+        if product.stock != 0
+          products_with_stock.push(product)
         end
-        product
+      end
+      products_with_stock
     end
 end
