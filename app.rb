fba9cfa071e4813f9358d87ade5928f834a9dcbd
Nimna Ekanayaka <nimna@nimna.me>
2016-05-10 09:12:51 -0300

    Updated so correct arguments are passed to the respective classes.

--- app.rb
+++ app.rb
@@ -5,13 +5,14 @@ require_relative "lib/transaction"
 
 # PRODUCTS
 
-Product.new(title: "LEGO Iron Man vs. Ultron", price: 22.99, stock: 55)
-Product.new(title: "Nano Block Empire State Building", price: 49.99, stock: 12)
-Product.new(title: "LEGO Firehouse Headquarter", price: 199.99, stock: 0)
+# I had to modify the existing code according to a previous reviewer's suggestion.
+Product.new("LEGO Iron Man vs. Ultron", price: 22.99, stock: 55)
+Product.new("Nano Block Empire State Building", price: 49.99, stock: 12)
+Product.new("LEGO Firehouse Headquarter", price: 199.99, stock: 0)
 
 puts Product.all.count # Should return 3
 
-Product.new(title: "LEGO Iron Man vs. Ultron", price: 22.99, stock: 55)
+Product.new("LEGO Iron Man vs. Ultron", price: 22.99, stock: 55)
 # Should return DuplicateProductError: 'LEGO Iron Man vs. Ultron' already exists.
 
 nanoblock = Product.find_by_title("Nano Block Empire State Building")
@@ -30,12 +31,14 @@ puts products_in_stock.include?(firehouse) # Should return false
 
 # CUSTOMERS
 
-Customer.new(name: "Walter Latimer")
-Customer.new(name: "Julia Van Cleve")
+# I had to modify the existing code according to a previous reviewer's suggestion.
+Customer.new("Walter Latimer")
+Customer.new("Julia Van Cleve")
 
 puts Customer.all.count # Should return 2
 
-Customer.new(name: "Walter Latimer")
+# I had to modify the existing code according to a previous reviewer's suggestion.
+Customer.new("Walter Latimer")
 # Should return DuplicateCustomerError: 'Walter Latimer' already exists.
 
 walter = Customer.find_by_name("Walter Latimer")
